f6e7a2f27b8c3fd63dcfbcbbb2d5c79f229b6d08
faust1002 <faust1002@gmail.com>
2020-06-03 10:00:47 +0100

    Reference model - refactor the initial sketch

--- Reference_Model/model.py
+++ Reference_Model/model.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3.6
 
 import numpy as np
-import scipy
-import scipy.signal
+import scipy.fft as fft
+import scipy.signal as signal
 import matplotlib
 import matplotlib.pyplot as plt
 
-def plot_signal_in_time_and_freq_domain(fs, time, signal):
+def plot_signal_in_time_and_freq_domain(time, test_vector, freq, spectrum):
     plt.subplot(1, 2, 1)
-    plt.plot(time * 1e3, signal)
+    plt.plot(time * 1e3, test_vector)
     plt.title("Signal + noise")
     plt.xlabel("Time [ms]")
     plt.ylabel("Voltage [V]")
     plt.grid()
 
     plt.subplot(1, 2, 2)
-    nfft = 4098
-    spectrum = scipy.fft.rfft(signal, nfft)
-    spectrum = np.abs(spectrum)
-    spectrum = spectrum * 2 / nfft
-    freq = scipy.fft.rfftfreq(nfft, 1 / fs) / 1e3
 
     plt.plot(freq, spectrum)
     plt.title("Fourier transform")
@@ -29,26 +24,41 @@ def plot_signal_in_time_and_freq_domain(fs, time, signal):
 
     plt.show()
 
+def generate_test_vector(fcarrier, fs):
+    f0 = fcarrier / 2
+    f1 = fcarrier * 2
+    f2 = fcarrier * 3
+    fmax = np.max([fcarrier, f0, f1, f2])
+    t = np.arange(0, 100 * 1 / fmax, 1 / fs)
+    test_vector = (5 * np.sin(2 * np.pi * f0 * t) +
+                   10 * np.sin(2 * np.pi * fcarrier * t) +
+                   20 * np.sin(2 * np.pi * f1 * t) +
+                   30 * np.sin(2 * np.pi * f2 * t))
+    return (t, test_vector)
+
+def calculate_fft(test_vector, fs, nfft):
+    spectrum = fft.rfft(test_vector, nfft)
+    spectrum = np.abs(spectrum)
+    spectrum = spectrum * 2 / nfft
+    freq = fft.rfftfreq(nfft, 1 / fs) / 1e3
     return (spectrum, freq)
 
-def plot_filter_frequency_response(fs, fdelta, fcarrier):
+def add_noise(test_vector, target_snr):
+    test_vector_avr_power = 10 * np.log10(np.mean(test_vector ** 2))
+    target_snr = 15
+    print("Average test_vector power %.2fdBm, target SNR = %.2fdB" % (test_vector_avr_power, target_snr))
+    noise_avr_power = test_vector_avr_power - target_snr
+    mean_noise = 0
+    noise = np.random.normal(mean_noise, np.sqrt(10 ** (noise_avr_power / 10)), len(test_vector))
+    test_vector += noise
+    return test_vector
+
+def bandpass_filter(test_vector, fs, fcarrier, fdelta):
     fpass = np.array([fcarrier - fdelta, fcarrier + fdelta])
     fstop = np.array([fcarrier - 2 * fdelta, fcarrier + 2 * fdelta])
-    print("fcarrier = %.2fkHz, fs = %.2fkHz, fdelta = %.2fkHz, fpass = %skHz, fstop = %skHz" % (fcarrier / 1e3, fs / 1e3, fdelta / 1e3, fpass / 1e3, fstop / 1e3))
-    N, Wn = scipy.signal.buttord(fpass, fstop, 3, 40, fs = fs)
-    print("Filter order = %d" % N);
-    b, a = scipy.signal.butter(N, Wn, "bandpass", fs = fs)
-    w, h = scipy.signal.freqz(b, a)
-    plt.semilogx(w, 20 * np.log10(np.abs(h)))
-    plt.title("Filter frequency response")
-    plt.xlabel("Frequency [kHz]")
-    plt.ylabel("Amplitude [dB]")
-    plt.margins(0, 0.1)
-    plt.grid(which = "both", axis = "both")
-
-    plt.show()
-
-    return (N, Wn)
+    N, Wn = signal.buttord(fpass, fstop, 3, 40, fs = fs)
+    sos = signal.butter(N, Wn, "bandpass", output = "sos", fs = fs)
+    return signal.sosfilt(sos, test_vector)
 
 def main():
     print("Hello")
@@ -56,36 +66,22 @@ def main():
     matplotlib.use("Qt5Agg")
     plt.style.use("dark_background")
 
-    f0 = 60e3
     fcarrier = 77.5e3
-    f2 = 77.5e3 * 2
-    f3 = 77.5e3 * 3
-    fmax = np.max([f0, fcarrier, f2, f3])
     fs = 1e6
+    time, test_vector = generate_test_vector(fcarrier, fs)
 
-    t = np.arange(0, 100 * 1 / fmax, 1 / fs)
-    signal = (5 * np.sin(2 * np.pi * f0 * t) +
-              10 * np.sin(2 * np.pi * fcarrier * t) +
-              20 * np.sin(2 * np.pi * f2 * t) +
-              30 * np.sin(2 * np.pi * f3 * t))
-
-    signal_avr_power = 10 * np.log10(np.mean(signal ** 2))
     target_snr = 15
-    print("Average signal power %.2fdBm, target SNR = %.2fdB" % (signal_avr_power, target_snr))
-    noise_avr_power = signal_avr_power - target_snr
-    mean_noise = 0
-    noise = np.random.normal(mean_noise, np.sqrt(10 ** (noise_avr_power / 10)), len(t))
-    signal += noise
+    test_vector = add_noise(test_vector, target_snr)
 
-    plot_signal_in_time_and_freq_domain(fs, t, signal)
+    nfft = 4098
+    spectrum, freq = calculate_fft(test_vector, fs, nfft)
+    plot_signal_in_time_and_freq_domain(time, test_vector, freq, spectrum)
 
     fdelta = 5e3
-    N, Wn = plot_filter_frequency_response(fs, fdelta, fcarrier)
+    test_vector = bandpass_filter(test_vector, fs, fcarrier, fdelta)
 
-    sos = scipy.signal.butter(N, Wn, "bandpass", output = "sos", fs = fs)
-    signal = scipy.signal.sosfilt(sos, signal)
-
-    spectrum, freq = plot_signal_in_time_and_freq_domain(fs, t, signal)
+    spectrum, freq = calculate_fft(test_vector, fs, nfft)
+    plot_signal_in_time_and_freq_domain(time, test_vector, freq, spectrum)
 
     peek = freq[np.argmax(spectrum)]
     print("Found peek = %.2f" % peek)
