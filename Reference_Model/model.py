091bf0837c011e4eaa19725fa2c45c1bd1994a8c
faust1002 <faust1002@gmail.com>
2020-06-03 10:19:58 +0100

    Reference modem - replace dummy print function with a proper logging

--- Reference_Model/model.py
+++ Reference_Model/model.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3.6
 
+import logging
 import numpy as np
 import scipy.fft as fft
 import scipy.signal as signal
 import matplotlib
 import matplotlib.pyplot as plt
+
+def configure_logging(logfile):
+    formatter = logging.Formatter("[%(asctime)s] %(levelname)s - %(message)s")
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.INFO)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    handler = logging.FileHandler(logfile, mode='w')
+    handler.setLevel(logging.DEBUG)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
 def plot_signal_in_time_and_freq_domain(time, test_vector, freq, spectrum):
     plt.subplot(1, 2, 1)
@@ -46,7 +62,7 @@ def calculate_fft(test_vector, fs, nfft):
 def add_noise(test_vector, target_snr):
     test_vector_avr_power = 10 * np.log10(np.mean(test_vector ** 2))
     target_snr = 15
-    print("Average test_vector power %.2fdBm, target SNR = %.2fdB" % (test_vector_avr_power, target_snr))
+    logging.info("Average test_vector power %.2fdBm, target SNR = %.2fdB" % (test_vector_avr_power, target_snr))
     noise_avr_power = test_vector_avr_power - target_snr
     mean_noise = 0
     noise = np.random.normal(mean_noise, np.sqrt(10 ** (noise_avr_power / 10)), len(test_vector))
@@ -57,12 +73,14 @@ def bandpass_filter(test_vector, fs, fcarrier, fdelta):
     fpass = np.array([fcarrier - fdelta, fcarrier + fdelta])
     fstop = np.array([fcarrier - 2 * fdelta, fcarrier + 2 * fdelta])
     N, Wn = signal.buttord(fpass, fstop, 3, 40, fs = fs)
+    logging.info("Bandpass: %skHz, stopband: %skHz, filter order = %d" % ((str(fpass / 1e3), str(fstop / 1e3), N)))
     sos = signal.butter(N, Wn, "bandpass", output = "sos", fs = fs)
     return signal.sosfilt(sos, test_vector)
 
 def main():
     print("Hello")
 
+    configure_logging("log.txt")
     matplotlib.use("Qt5Agg")
     plt.style.use("dark_background")
 
@@ -84,7 +102,7 @@ def main():
     plot_signal_in_time_and_freq_domain(time, test_vector, freq, spectrum)
 
     peek = freq[np.argmax(spectrum)]
-    print("Found peek = %.2f" % peek)
+    logging.info("Found peek = %.2fkHz" % peek)
 
     print("Goodbye")
 
