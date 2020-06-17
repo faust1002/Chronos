a0587fa09a9ab28ac850e4239d5ec22c28c8e9f8
faust1002 <faust1002@gmail.com>
2020-06-17 18:26:06 +0100

    Reference model - add phase modulation

--- Reference_Model/TestVectorGenerator.py
+++ Reference_Model/TestVectorGenerator.py
@@ -6,8 +6,12 @@ class TestVectorGenerator:
     __fs               = None
     __fcarrier         = None
     __target_snr       = None
+    __offset           = 0
+    __chip_length      = 0
+    __prsk_length      = 512
     __prsk             = [None, None]
     __bits             = [None, None]
+    __phasors          = [None, None]
 
     __signal           = None
     __time             = None
@@ -16,14 +20,22 @@ class TestVectorGenerator:
         self.__fs               = fs
         self.__fcarrier         = fcarrier
         self.__target_snr       = target_snr
-        self.__generate_prsk()
+        self.__offset           = int(fs / 10)
+        self.__chip_length      = int(1.55e-3 * fs)
         self.__generate_bits()
+        self.__generate_prsk()
+        self.__generate_phasors()
+
+    def __generate_bits(self):
+        self.__bits[0] = np.ones(int(self.__fs), dtype=np.complex128)
+        self.__bits[0][0 : self.__offset] = 0.15
+        self.__bits[1] = np.ones(int(self.__fs), dtype=np.complex128)
+        self.__bits[1][0 : 2 * self.__offset] = 0.15
 
     def __generate_prsk(self):
         lsfr = 0
-        seq_len = 512
-        chip_sequence = np.zeros(seq_len, dtype=int)
-        for idx in range(seq_len):
+        chip_sequence = np.zeros(self.__prsk_length, dtype=int)
+        for idx in range(self.__prsk_length):
             chip = lsfr & 1
             chip_sequence[idx] = chip
             lsfr >>= 1
@@ -33,11 +45,21 @@ class TestVectorGenerator:
         self.__prsk[0] = chip_sequence
         self.__prsk[1] = chip_sequence ^ 1
 
-    def __generate_bits(self):
-        self.__bits[0] = np.ones(int(self.__fs), dtype=np.complex128)
-        self.__bits[0][0 : int(self.__fs / 10)] = 0.15
-        self.__bits[1] = np.ones(int(self.__fs), dtype=np.complex128)
-        self.__bits[1][0 : int(self.__fs / 20)] = 0.15
+    def __generate_phasors(self):
+        phase_shift = 13 * np.pi / 180
+        self.__phasors[0] = np.exp(1j * phase_shift)
+        self.__phasors[1] = np.exp(-1j * phase_shift)
+
+    def __encode_one_bit(self, bit_idx, bit_value):
+        start = int(bit_idx * self.__fs)
+        end = int((bit_idx + 1) * self.__fs)
+        signal = self.__signal[start : end]
+        signal *= self.__bits[bit_value]
+        prsk = self.__prsk[bit_value]
+        for idx in range(self.__prsk_length):
+            start = 2 * self.__offset + idx * self.__chip_length
+            end = 2 * self.__offset + (idx + 1) * self.__chip_length
+            signal[start : end] *= self.__phasors[prsk[idx]]
 
     def __encode_bitstream(self, bitstream):
         logging.info("Encoding bitstream = %s" % str(bitstream))
@@ -45,9 +67,7 @@ class TestVectorGenerator:
         self.__signal = np.exp(1j * 2 * np.pi * self.__fcarrier * self.__time)
 
         for idx in range(len(bitstream)):
-            start = int(idx * self.__fs)
-            end = int((idx + 1) * self.__fs)
-            self.__signal[start : end] *= self.__bits[bitstream[idx]]
+            self.__encode_one_bit(idx, bitstream[idx])
 
         self.__signal = np.imag(self.__signal)
 
